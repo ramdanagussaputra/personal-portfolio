caa909dd4787ac5bbc547cf0e6af81bcd623d4b9
Ramdan Agus Saputra <ramdan@massivemusic.co.id>
2024-09-01 17:03:27 +0700

    install lucide icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { Button } from "antd"
+import { Github } from "lucide-react"
 
 export default function Home() {
   return (
     <div>
       <h1>Title</h1>
+      <Github className="text-red-200 text-2xl"/>
     </div>
   )
 }
